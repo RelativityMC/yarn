529366ae56fa46f6af44c45c36d2ccfaa7eed26e
ishland <30385023+ishland@users.noreply.github.com>
2026-08-17 22:46:16 +0800

    fix: checkstyle

--- filament/src/main/java/net/fabricmc/filament/task/ExtractMappingsTask.java
+++ filament/src/main/java/net/fabricmc/filament/task/ExtractMappingsTask.java
@@ -1,23 +1,25 @@
 package net.fabricmc.filament.task;
 
-import net.fabricmc.filament.task.base.FilamentTask;
-import net.fabricmc.filament.task.base.WithFileOutput;
-
-import net.fabricmc.loom.util.ZipUtils;
-
-import org.gradle.api.file.ConfigurableFileCollection;
-import org.gradle.api.tasks.InputFiles;
-import org.gradle.api.tasks.TaskAction;
-import org.gradle.work.DisableCachingByDefault;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
 
+import org.gradle.api.file.ConfigurableFileCollection;
+import org.gradle.api.tasks.InputFiles;
+import org.gradle.api.tasks.PathSensitive;
+import org.gradle.api.tasks.PathSensitivity;
+import org.gradle.api.tasks.TaskAction;
+import org.gradle.work.DisableCachingByDefault;
+
+import net.fabricmc.filament.task.base.FilamentTask;
+import net.fabricmc.filament.task.base.WithFileOutput;
+import net.fabricmc.loom.util.ZipUtils;
+
 @DisableCachingByDefault
 public abstract class ExtractMappingsTask extends FilamentTask implements WithFileOutput {
 	@InputFiles
+	@PathSensitive(PathSensitivity.NONE)
 	public abstract ConfigurableFileCollection getInputFiles();
 
 	@TaskAction
